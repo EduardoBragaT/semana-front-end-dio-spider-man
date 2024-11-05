5867d6336e4d3b84f04f135e21f71b0a6e3198a1
EduardoBragaT <luixed18@gmail.com>
2024-11-05 16:01:39 -0400

    Home page

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,9 +1,11 @@
 function handleMouseEnter () {
     this.classList.add('spiderman-card--hovered');
+    document.body.id = `${this.id}-hovered` 
 }
 
 function handleMouseLeave () {
     this.classList.remove('spiderman-card--hovered');
+    document.body.id = `` 
 }
 
 function addEventListenersToCards() {
